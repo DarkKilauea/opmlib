eddd29751bfe962d564ae1e406d176e8aeb33c40
Joshua Jones <kilauea.jones@gmail.com>
2014-11-21 20:37:11 -0600

    Initial add of project.

---
+++ opmlib/opmlib.h
@@ -0,0 +1,19 @@
+//
+//  opmlib.h
+//  opmlib
+//
+//  Created by Joshua Jones on 11/21/14.
+//  Copyright (c) 2014 Joshua Jones. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for opmlib.
+FOUNDATION_EXPORT double opmlibVersionNumber;
+
+//! Project version string for opmlib.
+FOUNDATION_EXPORT const unsigned char opmlibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <opmlib/PublicHeader.h>
+
+
